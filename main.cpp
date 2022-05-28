4f5040137028cf6a10f9391fe7a326aa943eb8a5
ItsPops <brille.francois@gmail.com>
2022-05-28 13:36:07 +0200

    fixed scores that were reset after each games

--- main.cpp
+++ main.cpp
@@ -4,28 +4,34 @@
 #include "jeu.h"
 #include "menu.h"
 
-
 using namespace std;
 
 
 int main () {
     bool continuer{true};
     auto indiceCourant{0};
+    auto scores = array<int, NOMBRE_SCORES>{0,0,0}; 
     while (continuer) {
         afficherMenu();
         cout << WELCOME_DIALOG << endl;
-        auto scores = array<int, NOMBRE_SCORES>{1,2,3}; 
         //array <int, NOMBRE_SCORES> scores {1,2,3};
         
         auto choix = demanderChoixMenu();
         switch (choix) {
         case ChoixMenu::JOUER:
+            
             scores[indiceCourant] = jeu(4598, BORNE_MAX);
-            //TEST: cout << jeu(4598, BORNE_MAX) << "test" << endl;
-            //TEST: cout << scores[0] << "TEST" << endl; //TEST
-            indiceCourant = (indiceCourant + 1 ) % NOMBRE_SCORES;
-            //indiceCourant++;
+            //while (indiceCourant <= 3) {
+                //indiceCourant ++;
+                //TEST: cout << jeu(4598, BORNE_MAX) << "test" << endl;
+                cout << scores[0] << " TEST APRES PARTIE" << endl; //TEST
+                indiceCourant = (indiceCourant + 1 ) % NOMBRE_SCORES;
             break;
+            // if (indiceCourant == 3) {
+            //     indiceCourant = 0;
+            //     }
+            // } 
+            // break;
         case ChoixMenu::AFFICHER_SCORES:
             for(auto i{0}; i < NOMBRE_SCORES; i++) {
                 cout << scores[i] << endl;
